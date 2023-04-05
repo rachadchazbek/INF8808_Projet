800c3754cbd01cf2ff3fd21f5125ecb9a7c7e7c6
steven duchene <steven.duchene@polymtl.ca>
2023-04-04 20:56:49 -0400

    Deleted extra file

--- code/src/pages/PhysicalHabits/physical-habits-tab.js
+++ code/src/pages/PhysicalHabits/physical-habits-tab.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import MaleAgeBoxAndWhisker from './MaleBoxAndWhisker/MaleAgeBoxAndWhisker'
-import MaleHeightBoxAndWhisker from './MaleBoxAndWhisker/MaleHeightBoxAndWhisker'
 
 const PhysicalHabitsPage = () => {
   return (
