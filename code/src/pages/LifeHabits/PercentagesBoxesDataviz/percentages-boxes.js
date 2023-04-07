bdeb8859fc45362764e7469bc76793740de639b6
rachadchazbek17 <rachadchazbek@gmail.com>
2023-04-07 12:21:26 -0400

    added calculate percentages

--- code/src/pages/LifeHabits/PercentagesBoxesDataviz/percentages-boxes.js
+++ code/src/pages/LifeHabits/PercentagesBoxesDataviz/percentages-boxes.js
@@ -9,6 +9,9 @@ const PercentagesBoxes = () => {
         const fetchData = async () => {
             d3.csv(DATA_PATH).then(data => {
                 const processedData = processData(data);
+                const percentages = calculatePercentages(processedData);
+                console.log(percentages)
+
             })
         }
 
@@ -16,6 +19,11 @@ const PercentagesBoxes = () => {
     })
 
     const processData = (data) => {
+        // Filter out people who do not have cardiovascular disease
+        data = data.filter(d => d.cardio === '1');
+        console.log(data.length)
+
+        // Transform smoke and alco to boolean values and return the processed data
         return data.map(d => {
             return {
                 smoker: d.smoke === '1',
@@ -24,6 +32,27 @@ const PercentagesBoxes = () => {
         })
     }
 
+    const calculatePercentages = (processedData) => {
+        const total = processedData.length;
+
+        const nonSmokersNonDrinkers = processedData.filter(d => !d.smoker && !d.drinker).length;
+        const smokersNonDrinkers = processedData.filter(d => d.smoker && !d.drinker).length;
+        const nonSmokersDrinkers = processedData.filter(d => !d.smoker && d.drinker).length;
+        const smokersDrinkers = processedData.filter(d => d.smoker && d.drinker).length;
+
+        const nonSmokersNonDrinkersPercentage = Math.round(nonSmokersNonDrinkers / total * 100);
+        const smokersNonDrinkersPercentage = Math.round(smokersNonDrinkers / total * 100);
+        const nonSmokersDrinkersPercentage = Math.round(nonSmokersDrinkers / total * 100);
+        const smokersDrinkersPercentage = Math.round(smokersDrinkers / total * 100);
+
+        return [
+            nonSmokersNonDrinkersPercentage,
+            smokersNonDrinkersPercentage,
+            nonSmokersDrinkersPercentage,
+            smokersDrinkersPercentage,
+        ]
+    }
+    
   return (
     <div>percentages-boxes</div>
   )
