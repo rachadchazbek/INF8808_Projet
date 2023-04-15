65221b6cd2149854de5870883ae3bdf5019a35c9
rachadchazbek17 <rachadchazbek@gmail.com>
2023-04-14 20:32:25 -0400

    Add each boxe is a seperate svg

--- code/src/pages/LifeHabits/PercentagesBoxesDataviz/percentages-boxes.js
+++ code/src/pages/LifeHabits/PercentagesBoxesDataviz/percentages-boxes.js
@@ -4,10 +4,6 @@ import * as d3 from 'd3'
 import {DATA_PATH} from '../../../constants/paths'
 
 const PercentagesBoxes = () => {
-    const width = 750;
-    const height = 150;
-    const boxWidth = 150;
-
     useEffect(() => {
         const fetchData = async () => {
             d3.csv(DATA_PATH).then(data => {
@@ -15,9 +11,10 @@ const PercentagesBoxes = () => {
                 const percentages = calculatePercentages(processedData);
                 console.log(percentages);
 
-                const svg = createSVG();
-                appendBoxes(svg, percentages);
-                appendText(svg, percentages);
+                // const svg = createSVG();
+                // appendBoxes(svg, percentages);
+                // appendText(svg, percentages);
+                createDataviz(percentages)
             })
         }
         fetchData()
@@ -57,59 +54,144 @@ const PercentagesBoxes = () => {
         ]
     }
 
-    const scalingFactor = 0.75;
-    const boxSizes = [100, 0, 0, 0];
-    for (let i = 1; i < boxSizes.length; i++) {
-      boxSizes[i] = boxSizes[i - 1] * scalingFactor;
+    const createDataviz = (percentages) => {
+      const box1 = d3.select('#box1')
+        .append('svg')
+        .attr('width', 200)
+        .attr('height', 200)
+        .style('background-color', 'red')
+        
+        box1
+        .append('text')
+        .text(percentages[0] + '%')
+        .attr('x', 200 / 2)
+        .attr('y', 200 / 2)
+        .attr('text-anchor', 'middle')
+        .style('font-size', '25px')
+        .style('font-weight', 'bold')
+
+        box1
+        .append('text')
+        .attr('x', 200 / 2)
+        .attr('y', 200 / 2 + 5)
+        .attr('text-anchor', 'middle')
+        .style('font-size', '11px')
+        .style('font-weight', 'bold')
+        .selectAll('tspan')
+        .data(['Not Smokers', 'and Not Drinkers'])
+        .enter()
+        .append('tspan')
+        .attr('x', 200 / 2)
+        .attr('dy', '1.2em') // set the line spacing to 1.2em
+        .text(function(d) { return d; });
+
+
+
+      const box2 = d3.select('#box2')
+        .append('svg')
+        .attr('width', 150)
+        .attr('height', 150)
+        .style('background-color', 'red')
+
+        box2
+        .append('text')
+        .text(percentages[1] + '%')
+        .attr('x', 150 / 2)
+        .attr('y', 150 / 2)
+        .attr('text-anchor', 'middle')
+        .style('font-size', '25px')
+        .style('font-weight', 'bold')
+
+        box2
+        .append('text')
+        .attr('x', 150 / 2)
+        .attr('y', 150 / 2 + 5)
+        .attr('text-anchor', 'middle')
+        .style('font-size', '11px')
+        .style('font-weight', 'bold')
+        .selectAll('tspan')
+        .data(['Smokers', 'and Not Drinkers'])
+        .enter()
+        .append('tspan')
+        .attr('x', 150 / 2)
+        .attr('dy', '1.2em') // set the line spacing to 1.2em
+        .text(function(d) { return d; });
+        
+      const box3 = d3.select('#box3')
+        .append('svg')
+        .attr('width', 100)
+        .attr('height', 100)
+        .style('background-color', 'red')
+
+        box3
+        .append('text')
+        .text(percentages[2] + '%')
+        .attr('x', 100 / 2)
+        .attr('y', 100 / 2)
+        .attr('text-anchor', 'middle')
+        .style('font-size', '25px')
+        .style('font-weight', 'bold')
+                // Add text : fument et boivent under each percentage
+
+        box3
+        .append('text')
+        .attr('x', 100 / 2)
+        .attr('y', 100 / 2 + 5)
+        .attr('text-anchor', 'middle')
+        .style('font-size', '11px')
+        .style('font-weight', 'bold')
+
+        .selectAll('tspan')
+        .data(['Not Smokers', 'and Drinkers'])
+        .enter()
+        .append('tspan')
+        .attr('x', 100 / 2)
+        .attr('dy', '1.2em') // set the line spacing to 1.2em
+        .text(function(d) { return d; });
+
+      const box4 = d3.select('#box4')
+        .append('svg')
+        .attr('width', 75)
+        .attr('height', 75)
+        .style('background-color', 'red')
+        // Add text : 50% fument et boivent
+
+        box4
+        .append('text')
+        .text(percentages[3] + '%')
+        .attr('x', 75 / 2)
+        .attr('y', 75 / 2)
+        .attr('text-anchor', 'middle')
+        .style('font-size', '25px')
+        .style('font-weight', 'bold')
+
+        box4
+        .append('text')
+        .attr('x', 75 / 2)
+        .attr('y', 75 / 2 + 5)
+        .attr('text-anchor', 'middle')
+        .style('font-size', '11px')
+        .style('font-weight', 'bold')
+
+        .selectAll('tspan')
+        .data(['Smokers', 'and Drinkers'])
+        .enter()
+        .append('tspan')
+        .attr('x', 75 / 2)
+        .attr('dy', '1.2em') // set the line spacing to 1.2em
+        .text(function(d) { return d; });
     }
-    
-    const boxMargin = 25;
-    
-    const createSVG = () => {
-      return d3
-        .select(".percentages-boxes-graph")
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height);
-    };
-    
-    const appendBoxes = (svg, percentages) => {
-      const maxPercentage = d3.max(boxSizes);
-    
-      svg
-        .selectAll("rect")
-        .data(boxSizes)
-        .enter()
-        .append("rect")
-        .attr("x", (d, i) => i * (boxWidth + boxMargin))
-        .attr("y", (d, i) => (height - boxSizes[i]) / 2)
-        .attr("width", (d, i) => boxSizes[i])
-        .attr("height", (d, i) => boxSizes[i])
-        .style("fill", "red")
-        .style("opacity", (d, i) => (i + 1) / boxSizes.length);
-    };
-    
-    const appendText = (svg, percentages) => {
-      svg
-        .selectAll("text")
-        .data(percentages)
-        .enter()
-        .append("text")
-        .text((d) => `${d}%`)
-        .attr("x", (d, i) => i * (boxWidth + boxMargin) + boxWidth / 2)
-        .attr("y", height / 2 + 5)
-        .attr("text-anchor", "middle")
-        .style("font-size", "10px");
-    };
-    
-    const svg = createSVG();
-    appendBoxes(svg, [25, 50, 75, 100]);
-    appendText(svg, [25, 50, 75, 100]);
-    
+
 
   return (
-    <div className='percentages-boxes-graph'></div>
-  )
+
+    <div class='boxes-container'>
+      <div id='box1'></div>
+      <div id='box2'></div>
+      <div id='box3'></div>
+      <div id='box4'></div>
+    </div>
+    )
 }
 
 export default PercentagesBoxes
